d4e1a977b66a86c49fa6e1ee6bd62e14de6c9ae7
root <root@localhost.localdomain>
2015-10-13 23:58:25 +0000

    added shell of file reading

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,8 +1,8 @@
 require 'packetfu'
 
-NULL_SCAN = 0
-FIN_SCAN = 1
-XMAS_SCAN = 41
+NULL_FLAGS = 0
+FIN_FLAGS = 1
+XMAS_FLAGS = 41
 
 def print_incident(num, inc, src, prot, pay)
 	inc_string = "#" + num.to_s()
@@ -13,26 +13,36 @@ def print_incident(num, inc, src, prot, pay)
 	puts inc_string
 end
 
-pkts = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
-incidents = 0
+if ARGV.length == 2 then
+	if ARGV[0] != "-r" then
+		puts "USAGE: ruby alarm.rb [-r] [log_file]"
+	else
+		puts "ur doin it!"
+	end
 
-pkts.stream.each do |p|
-	pkt = PacketFu::Packet.parse(p)
-	
-	if pkt.is_ip? then
-		if pkt.is_tcp? then
-			if pkt.tcp_flags.to_i() == NULL_SCAN then
-				print_incident(incidents, "NULL Scan", pkt.ip_saddr, "TCP", pkt.payload())
-				incidents += 1
-			elsif pkt.tcp_flags.to_i() == FIN_SCAN then
-				print_incident(incidents, "FIN Scan", pkt.ip_saddr, "TCP", pkt.payload())
-				incidents += 1
-			elsif pkt.tcp_flags.to_i() == XMAS_SCAN then
-				print_incident(incidents, "XMAS Scan", pkt.ip_saddr, "TCP", pkt.payload())
-				incidents += 1
-			end		
+else
+
+	pkts = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
+	incidents = 0
+
+	pkts.stream.each do |p|
+		pkt = PacketFu::Packet.parse(p)
+		
+		if pkt.is_ip? then
+			if pkt.is_tcp? then
+				puts "Payload: " + pkt.payload
+				if pkt.tcp_flags.to_i() == NULL_FLAGS then
+					print_incident(incidents, "NULL Scan", pkt.ip_saddr, "TCP", pkt.payload())
+					incidents += 1
+				elsif pkt.tcp_flags.to_i() == FIN_FLAGS then
+					print_incident(incidents, "FIN Scan", pkt.ip_saddr, "TCP", pkt.payload())
+					incidents += 1
+				elsif pkt.tcp_flags.to_i() == XMAS_FLAGS then
+					print_incident(incidents, "XMAS Scan", pkt.ip_saddr, "TCP", pkt.payload())
+					incidents += 1
+				end		
+			end
 		end
 	end
 end
-
 
